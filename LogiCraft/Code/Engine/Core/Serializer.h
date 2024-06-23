70078a0b25b1677fe821d81eedbc143fdae4fbfa
sharllesse <charles19lesage06@gmail.com>
2024-06-24 01:31:48 +0200

    Added Json Lib + Updated CMake
    - Added nlohmann json lib https://github.com/nlohmann/json and i made this lib as a static lib plus i updated the cmake to have it in the engine.

---
+++ LogiCraft/Code/Engine/Core/Serializer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <define.h>
+
+#include <fstream>
+#include <string>
+
+class Serializer
+{
+public:
+	Serializer() = default;
+	~Serializer() = default;
+private:
+	//STATIC OU PAS ?
+};
