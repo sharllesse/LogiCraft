c195c1faed2d0f48c7c91a15a1b7c694d201bb60
sharllesse <charles19lesage06@gmail.com>
2024-06-24 18:13:18 +0200

    Update Serializer.h

--- LogiCraft/Code/Engine/Core/Serializer.h
+++ LogiCraft/Code/Engine/Core/Serializer.h
@@ -14,23 +14,56 @@ public:
 	Serializer(Serializer&& other);
 	~Serializer() = default;
 
+	Serializer& operator=(const Serializer& other);
+	Serializer& operator=(Serializer&& other);
+
+	json& operator[](const std::string& key) { return m_json[key]; }
+
+	bool operator!=(const Serializer& other) const { return m_json != other.m_json; }
+	bool operator==(const Serializer& other) const { return m_json == other.m_json; }
+
 	bool Parse(const std::string& filename);
 
+	/*
+	 * @brief Return the iterator to the beginning of the json objects
+	 */
 	json::iterator Begin() { return m_json.begin(); }
+
+	/*
+	 * @brief Return the iterator to the end of the json objects
+	 */
 	json::iterator End() { return m_json.end(); }
 
+	/*
+	 * @brief Return the const iterator to the beginning of the json objects
+	 */
 	json::const_iterator ConstBegin() const { return m_json.cbegin(); }
+	/*
+	 * @brief Return the const iterator to the end of the json objects
+	 */
 	json::const_iterator ConstEnd() const { return m_json.cend(); }
 
+	/*
+	 * @brief Erase the iterator from the json objects
+	 */
 	json::iterator Erase(const json::iterator& it) { return m_json.erase(it); }
 
+	/*
+	 * @brief Remove the iterator from the json objects
+	 */
 	void Remove(const std::string& key) { m_json.erase(key); }
 
+	/*
+	 * @brief Swap the json objects with another json object
+	 */
 	void Swap(json& other) { m_json.swap(other); }
 
 	json::iterator Insert(const json::const_iterator& it, const json& value) { return m_json.insert(it, value); }
 	json::iterator Insert(const json::const_iterator& it, json&& value) { return m_json.insert(it, std::move(value)); }
 	json::iterator Insert(const json::const_iterator& it, size_t count, const json& value) { return m_json.insert(it, count, value); }
+
+	//void Emplace(const std::string& key, const json& value) { m_json.emplace(key, value); }
+	//void EmplaceBack(const std::string& key, const json& value) { m_json.emplace_back(key, value); }
 
 	json::iterator       Find(const std::string& key) { return m_json.find(key); }
 	json::const_iterator Find(const std::string& key) const { return m_json.find(key); }
@@ -55,13 +88,6 @@ public:
 	json& GetJson() { return m_json; }
 
 	std::vector<std::string> GetKeys() const;
-
-	json& operator[](const std::string& key) { return m_json[key]; }
-
-	bool        operator!=(const Serializer& other) const { return m_json != other.m_json; }
-	bool        operator==(const Serializer& other) const { return m_json == other.m_json; }
-	Serializer& operator=(const Serializer& other);
-	Serializer& operator=(Serializer&& other);
 
 private:
 	json m_json;
