ff977df08c644dd56e6197b0eb7ea61d6ce59bb4
sharllesse <charles19lesage06@gmail.com>
2024-06-24 17:43:23 +0200

    Made Serializer Class

--- LogiCraft/Code/Engine/Core/Serializer.cpp
+++ LogiCraft/Code/Engine/Core/Serializer.cpp
@@ -1,1 +1,41 @@
 #include "Serializer.h"
+
+Serializer::Serializer(Serializer&& other)
+{
+	m_json = std::move(other.m_json);
+}
+
+bool Serializer::Parse(const std::string& filename)
+{
+	std::ifstream file(filename);
+	if (file.is_open())
+	{
+		m_json = json::parse(file);
+
+		return true;
+	}
+
+	return false; //Could not open file
+}
+
+std::vector<std::string> Serializer::GetKeys() const
+{
+	std::vector<std::string> keys;
+
+	for (auto it = m_json.cbegin(); it != m_json.cend(); ++it)
+		keys.emplace_back(it.key());
+
+	return keys;
+}
+
+Serializer& Serializer::operator=(const Serializer& other)
+{
+	m_json = other.m_json;
+	return *this;
+}
+
+Serializer& Serializer::operator=(Serializer&& other)
+{
+	m_json = std::move(other.m_json);
+	return *this;
+}
--- LogiCraft/Code/Engine/Core/Serializer.h
+++ LogiCraft/Code/Engine/Core/Serializer.h
@@ -1,14 +1,68 @@
 #pragma once
-#include <define.h>
+#include "DLLExport.h"
+#include "json.hpp"
 
 #include <fstream>
 #include <string>
+
+using namespace nlohmann;
 
 class Serializer
 {
 public:
 	Serializer() = default;
+	Serializer(Serializer&& other);
 	~Serializer() = default;
+
+	bool Parse(const std::string& filename);
+
+	json::iterator Begin() { return m_json.begin(); }
+	json::iterator End() { return m_json.end(); }
+
+	json::const_iterator ConstBegin() const { return m_json.cbegin(); }
+	json::const_iterator ConstEnd() const { return m_json.cend(); }
+
+	json::iterator Erase(const json::iterator& it) { return m_json.erase(it); }
+
+	void Remove(const std::string& key) { m_json.erase(key); }
+
+	void Swap(json& other) { m_json.swap(other); }
+
+	json::iterator Insert(const json::const_iterator& it, const json& value) { return m_json.insert(it, value); }
+	json::iterator Insert(const json::const_iterator& it, json&& value) { return m_json.insert(it, std::move(value)); }
+	json::iterator Insert(const json::const_iterator& it, size_t count, const json& value) { return m_json.insert(it, count, value); }
+
+	json::iterator       Find(const std::string& key) { return m_json.find(key); }
+	json::const_iterator Find(const std::string& key) const { return m_json.find(key); }
+
+	template<typename T>
+	T At(const std::string& key)
+	{
+		return m_json.at(key).get<T>();
+	}
+
+	template<>
+	json At<json>(const std::string& key)
+	{
+		return m_json.at(key);
+	}
+
+	bool Contains(const std::string& key) const { return m_json.contains(key); }
+
+	size_t Size() const { return m_json.size(); }   // no Discard ?
+	bool   Empty() const { return m_json.empty(); } // Same here
+
+	json& GetJson() { return m_json; }
+
+	std::vector<std::string> GetKeys() const;
+
+	json& operator[](const std::string& key) { return m_json[key]; }
+
+	bool        operator!=(const Serializer& other) const { return m_json != other.m_json; }
+	bool        operator==(const Serializer& other) const { return m_json == other.m_json; }
+	Serializer& operator=(const Serializer& other);
+	Serializer& operator=(Serializer&& other);
+
 private:
-	//STATIC OU PAS ?
+	json m_json;
 };
